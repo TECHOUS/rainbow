c9ffbaaaffea41987de4ff96c7e29a0c4b93bf90
GauravWalia19 <gauravwalia019@gmail.com>
2020-01-13 01:22:34 +0530

    completed getting started section

--- docs/js/index.js
+++ docs/js/index.js
@@ -25,3 +25,25 @@ function openNavigation(){
 function closeNavigation(){
     rainbowNavScreen.style.height = "0%";
 }
+
+const copyToClipboard1 = document.getElementById('copyToClipboard1');
+copyToClipboard1.addEventListener('click',()=>{
+    const copyText = document.getElementById('copyText');
+    copyText.disabled = false;
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand("copy");
+    // alert("Copied the text: " + copyText.value);
+    copyText.disabled = true;
+})
+
+const copyToClipboard2 = document.getElementById('copyToClipboard2');
+copyToClipboard2.addEventListener('click',()=>{
+    const copyMvnText = document.getElementById('copyMvnText');
+    copyMvnText.disabled = false;
+    copyMvnText.select();
+    copyMvnText.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand("copy");
+    // alert("Copied the text: " + copyText.value);
+    copyMvnText.disabled = true;
+})
