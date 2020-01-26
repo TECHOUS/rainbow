61847a24521dc43e4a463df18fb6313f6f3ab0bb
GauravWalia19 <gauravwalia019@gmail.com>
2020-01-27 01:09:26 +0530

    completed developer use section

--- docs/js/index.js
+++ docs/js/index.js
@@ -47,3 +47,67 @@ copyToClipboard2.addEventListener('click',()=>{
     // alert("Copied the text: " + copyText.value);
     copyMvnText.disabled = true;
 })
+
+const copyToClipboard3 = document.getElementById('copyToClipboard3');
+copyToClipboard3.addEventListener('click',()=>{
+    const copyRainbowCode1 = document.getElementsByClassName('copyRainbowCode1');
+    let result = '';
+    for(let i=0;i<copyRainbowCode1.length ;i++){
+        copyRainbowCode1[i].disabled = false;
+        result = result + copyRainbowCode1[i].value + '\n';
+        copyRainbowCode1[i].disabled = true;
+    }
+    const textarea1 = document.getElementById('textarea1');
+    textarea1.value = result;
+    textarea1.select();
+    textarea1.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+})
+
+const copyToClipboard4 = document.getElementById('copyToClipboard4');
+copyToClipboard4.addEventListener('click',()=>{
+    const copyRainbowCode2 = document.getElementsByClassName('copyRainbowCode2');
+    let result = '';
+    for(let i=0;i<copyRainbowCode2.length ;i++){
+        copyRainbowCode2[i].disabled = false;
+        result = result + copyRainbowCode2[i].value + '\n';
+        copyRainbowCode2[i].disabled = true;
+    }
+    const textarea2 = document.getElementById('textarea2');
+    textarea2.value = result;
+    textarea2.select();
+    textarea2.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+})
+
+const copyToClipboard5 = document.getElementById('copyToClipboard5');
+copyToClipboard5.addEventListener('click',()=>{
+    const copyRainbowCode3 = document.getElementsByClassName('copyRainbowCode3');
+    let result = '';
+    for(let i=0;i<copyRainbowCode3.length ;i++){
+        copyRainbowCode3[i].disabled = false;
+        result = result + copyRainbowCode3[i].value + '\n';
+        copyRainbowCode3[i].disabled = true;
+    }
+    const textarea3 = document.getElementById('textarea3');
+    textarea3.value = result;
+    textarea3.select();
+    textarea3.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+})
+
+const copyToClipboard6 = document.getElementById('copyToClipboard6');
+copyToClipboard6.addEventListener('click',()=>{
+    const copyBashCode = document.getElementsByClassName('copyBashCode');
+    let result = '';
+    for(let i=0;i<copyBashCode.length ;i++){
+        copyBashCode[i].disabled = false;
+        result = result + copyBashCode[i].value + '\n';
+        copyBashCode[i].disabled = true;
+    }
+    const textarea4 = document.getElementById('textarea4');
+    textarea4.value = result;
+    textarea4.select();
+    textarea4.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+})
