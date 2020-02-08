2739e541e20161c6e33235fa16abf7a051b2f4c5
GauravWalia19 <gauravwalia019@gmail.com>
2020-02-09 02:58:21 +0530

    added colors docs in codes docs

--- docs/js/codes.js
+++ docs/js/codes.js
@@ -29,3 +29,47 @@ function closeNavigation(){
     const body = document.body;
     body.style.overflowY = 'scroll';
 }
+
+let init =()=>{
+    searchArray.push("RESET");
+    searchArray.push("BOLD");
+    searchArray.push("ITALIC");
+    searchArray.push("UNDERLINE");
+    searchArray.push("BLINK");
+    searchArray.push("HIDDEN");
+    searchArray.push("CUT");
+    searchArray.push("DOUBLELINE");
+    searchArray.push("OVERLINE");
+    searchArray.push("DEFAULT");
+    searchArray.push("LGREEN");
+    searchArray.push("MGREEN");
+    searchArray.push("DGREEN");
+    searchArray.push("LRED");
+    searchArray.push("DRED");
+    searchArray.push("LWHITE");
+    searchArray.push("DWHITE");
+    searchArray.push("LBLUE");
+    searchArray.push("DBLUE");
+    searchArray.push("LCYAN");
+    searchArray.push("DCYAN");
+    searchArray.push("ORANGE");
+    searchArray.push("BLACK");
+    searchArray.push("MAGENTA");
+    searchArray.push("YELLOW");
+    searchArray.push("LEMON");
+    searchArray.push("PINK");
+    searchArray.push("BGDGREEN");
+    searchArray.push("BGLGREEN");
+    searchArray.push("BGMGREEN");
+    searchArray.push("BGDRED");
+    searchArray.push("BGLRED");
+    searchArray.push("BGDORANGE");
+    searchArray.push("BGLORANGE");
+    searchArray.push("BGLBLUE");
+    searchArray.push("BGLWHITE");
+    searchArray.push("BGDWHITE");
+    searchArray.push("BGLEMON");
+    searchArray.push("BGMPINK");
+    searchArray.push("BGDPINK");
+    searchArray.push("BGLPINK");
+}
